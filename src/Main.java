d612d874287534d28d9c418ae084d4eb37796310
James McMeekin <jmcmeekin2025@student.amsacs.org>
2023-05-26 11:05:31 -0400

    chaching

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.*;
+import vars.GlobalVariables;
+
 public class Main {
     public static void main(String[] args) {
+        /*
+         * Scanner s = new Scanner(System.in);
+         * String difficulty = "";
+         * System.out.print("wsg blud do u want ez med or radiant diffucutly? ");
+         * difficulty = s.next();
+         * if (difficulty.charAt(0) == 'e' || difficulty.charAt(0) == 'E') {
+         * GlobalVariables.setDelay(150);
+         * System.out.println("booooooooo");
+         * } else if (difficulty.charAt(0) == 'm' || difficulty.charAt(0) == 'M') {
+         * GlobalVariables.setDelay(75);
+         * } else {
+         * GlobalVariables.setDelay(1);
+         * }
+         */
         Window window = new Window();
+
     }
 }
