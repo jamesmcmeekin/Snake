683b8a97667562e725746c87b4e62a586743d6b6
takoking2121 <76025523+takoking2121@users.noreply.github.com>
2023-06-08 21:00:11 -0400

    ..

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,6 @@ public class Main {
         }
         System.out.print("numba of apples? ");
         GlobalVariables.setApples(s.nextInt());
-        ;
         Window window = new Window();
 
     }
